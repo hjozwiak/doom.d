a03f9c75ce4eb8670e3484225b9a19b45a829d94
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-12-15 20:00:25 -0500

    Initial efforts. Set up Emacspea to (hopefully) work with Doom and enable some modules.

---
+++ esp-prepare.el
@@ -0,0 +1,5 @@
+;; In preparation for the loading of Emacspeak
+;; Set the speech rate, so as to not go insane.
+(setq espeak-default-speech-rate 820)
+
+(load-file "~/emacspeak/lisp/emacspeak-setup.el")
