c3eacea6b5eccd4aee0d2fb2b979d6f4f6559ed0
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-12-22 10:37:38 -0500

    Finish off the typo fixing. The original configuration works now.

--- config.el
+++ config.el
@@ -1,12 +1,12 @@
 ;; config.el, the configuration file for Doom Emacs.
 ;; Generated from config.org, do not manually edit.
 
-(add-hook 'after-init-hook (load '~/.doom.d/esp-prepare.el")
+(add-hook 'after-init-hook (load "~/.doom.d/esp-prepare.el"))
 
 (setq user-full-name "Hunter Jozwiak"
 user-mail-address "hunter.t.joz@gmail.com")
 
-(setq doom-theme 'doom-one')
+(setq doom-theme 'doom-one)
 
 (setq org-directory "~/org")
 
