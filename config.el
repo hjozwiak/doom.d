42607483e341bed218aa139d829f9d55e51b720a
Hunter Jozwiak <hunter.t.joz@gmail.com>
2021-12-25 10:15:35 -0500

    Finish off adding in my IRC servers and add an update script (rather primitive) to Emacspeak.

--- config.el
+++ config.el
@@ -11,3 +11,31 @@ user-mail-address "hunter.t.joz@gmail.com")
 (setq org-directory "~/org")
 
 (setq display-line-numbers-type nil)
+
+(after! circe
+  (set-irc-server! "irc.libera.chat"
+    `(:tls t
+      :port 6697
+      :nic "sektor"
+      :sasl-username ,(+pass-get-user "irc/libera.chat")
+      :sasl-password (lambda (&rest _) (+pass-get-secret "irc/libera.chat"))
+      :channels ("#emacs" "#stumpwm" "#pleroma"))))
+
+(after! circe
+  (set-irc-server! "irc.talkabout.cf"
+    `(:tls t
+      :port 6697
+      :nic "sektor"
+      :sasl-username ,(+pass-get-user "irc/talkabout.cf")
+      :sasl-password (lambda (&rest _) (+pass-get-secret "irc/talkabout.cf"))
+    :channels ("#a11y")))
+)
+
+(after! circe
+  (set-irc-server! "irc.zeronode.net"
+    `(:tls t
+      :port 6697
+      :nic "sektor"
+      :sasl-username ,(+pass-get-user "irc/zeronode.net")
+      :sasl-password (lambda (&rest _) (+pass-get-secret "irc/zeronode.net"))
+      :channels ("#noagenda"))))
---
+++ update-emacspeak.sh
@@ -0,0 +1,5 @@
+/usr/bin/bash
+cd ~/emacspeak
+git pull
+make clean
+make -j$(nproc --all)
