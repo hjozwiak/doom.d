0e182c5db400e275fc1d455cb12b7b7f12adf040
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-02-17 15:06:45 -0500

    Add the full search toggle to mu4e.

--- config.el
+++ config.el
@@ -58,4 +58,5 @@ user-mail-address "hunter.t.joz@gmail.com")
         send-mail-function #'smtpmail-send-it
         message-sendmail-f-is-evil t
         message-sendmail-extra-arguments '("--read-envelope-from")
-        message-send-mail-function #'message-send-mail-with-sendmail))
+        message-send-mail-function #'message-send-mail-with-sendmail
+  mu4e-headers-full-search t))
